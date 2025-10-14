fbeee961a06c459b3f9f1594fc6b7b4da4a81806
Hafizi Ruslan <30207509+HafiziRuslan@users.noreply.github.com>
2025-10-15 00:15:18 +0800

    🐛 fix(build): install dependencies after venv activation
    - ensures dependencies are installed within the activated virtual environment

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@ set -e
 if [ ! -d "venv" ]; then
     echo "Virtual environment not found. Creating one."
     python3 -m venv venv
-    echo "Installing dependencies"
-    pip install -r requirements.txt
 else
     echo "Virtual environment already exists."
 fi
 echo "Activating virtual environment"
 source venv/bin/activate
+echo "Installing dependencies"
+pip install -r requirements.txt
 echo "Running main.py"
 while true; do
     python3 ./main.py
